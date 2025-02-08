83fbf8ff15e05f10d2d8b5d6b14db8ef147c0cf2
vagnerpelais <vagnerpelais@gmail.com>
2025-02-08 11:36:01 -0300

    added clerk authrentication setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  return <h1 className="text-red-600">Hello world</h1>
+  return <Button>Hello world</Button>
 }
